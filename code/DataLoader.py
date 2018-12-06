c05ffcf092e3ff62696c680b4af2af785466f8cc
peterfengyx <peterfengyx@gmail.com>
2018-12-05 22:41:16 -0800

    new dataloader
    the one use package surprise

--- code/DataLoader.py
+++ code/DataLoader.py
@@ -13,6 +13,9 @@ import os
 import gzip
 import pickle
 from scipy.sparse import csr_matrix as sparse_matrix
+from surprise.prediction_algorithms.matrix_factorization import SVD, SVDpp, NMF
+from surprise import Dataset,Reader
+from random import sample
 
 def parse(path):
     g = gzip.open(path, 'r')
@@ -22,13 +25,16 @@ def parse(path):
 class DataLoader(object):
     def __init__(self,category,save_path):
         self.category=category
+        self.max_user=10000
         self.price_dict={}
         self.cate_dict={}
         self.top_value=15
+        self.model=NMF()
+        self.topk=500
         if not os.path.isfile(save_path):
             self.load_data()
-            self.create_user_item_matrix()
-            self.create_ratings(self.top_value)
+            #self.create_user_item_matrix()
+            self.create_ratings()
             self.save_data(save_path)
         else:
             self.load(save_path)
@@ -46,7 +52,9 @@ class DataLoader(object):
             try:
                 price=review['price']
                 asin=review['asin']
-                price_dict[asin]=price
+                v=list(review['salesRank'].values())[0]
+                if v<self.topk:
+                    price_dict[asin]=price
             except:
                 num_no_price+=1
                 continue
@@ -73,7 +81,7 @@ class DataLoader(object):
             except:
                 self.ratings=ratings_temp
         
-        
+    #old method
     def create_user_item_matrix(self, user_key="user",item_key="item"):
         n = len(set(self.ratings[user_key]))
         d = len(set(self.ratings[item_key]))
@@ -83,22 +91,36 @@ class DataLoader(object):
         self.user_inverse_mapper = dict(zip(list(range(n)), np.unique(self.ratings[user_key])))
         self.item_inverse_mapper = dict(zip(list(range(d)), np.unique(self.ratings[item_key])))
 
-        user_ind = [self.user_mapper[i] for i in self.ratings[user_key]]
-        item_ind = [self.item_mapper[i] for i in self.ratings[item_key]]
+        self.user_ind = [self.user_mapper[i] for i in self.ratings[user_key]]
+        self.item_ind = [self.item_mapper[i] for i in self.ratings[item_key]]
 
-        self.ratings_matrix = sparse_matrix((self.ratings["rating"], (user_ind, item_ind)), shape=(n,d))
+        self.ratings_matrix = sparse_matrix((self.ratings["rating"]-3, (self.user_ind, self.item_ind)), shape=(n,d))
         print("user-item matrix generated.")
-        
-    def create_ratings(self,top_value):
-        C=MBRecsys(self.ratings_matrix,top_value)
-        self.ratings_predict=C.predict()
+    
+    def create_ratings(self):
+        #C=MBRecsys(self.ratings_matrix,top_value)
+        S=set(self.ratings['user'])
+        S=sample(S,self.max_user)
+        n = len(S)
+        d = len(set(self.ratings['item']))
+        self.ratings=self.ratings[self.ratings['user'].isin(S)]
+        reader=Reader(rating_scale=(1,5))
+        data = Dataset.load_from_df(self.ratings[['user', 'item', 'rating']], reader)
+        train_set=data.build_full_trainset()
+        self.ratings_predict=np.zeros([n,d])
+        self.model.fit(train_set)
+        for i in train_set.all_users():
+            for j in train_set.all_items():
+                user_raw=train_set.to_raw_uid(i)
+                item_raw=train_set.to_raw_iid(j)
+                self.ratings_predict[i][j]=self.model.predict(user_raw, item_raw)[3]
         print("predicted ratings generated.")
         
     def save_data(self,save_path):
-        self.dict_all={'prices':self.price_dict,'raw_ratings':self.ratings_matrix,
-                           'new_ratings':self.ratings_predict,'cate':self.cate_dict,
-                           'user_mapper':self.user_mapper, 'item_mapper':self.item_mapper, 
-                           'user_inverse_mapper':self.user_inverse_mapper, 'item_inverse_mapper':self.item_inverse_mapper}
+        self.dict_all={'prices':self.price_dict,#'raw_ratings':self.ratings_matrix,
+                           'new_ratings':self.ratings_predict,'cate':self.cate_dict}
+                           #'user_mapper':self.user_mapper, 'item_mapper':self.item_mapper, 
+                           #'user_inverse_mapper':self.user_inverse_mapper, 'item_inverse_mapper':self.item_inverse_mapper}
         with open(save_path,'wb') as f:
             pickle.dump(self.dict_all, f)
         print("data saved in ", save_path)
@@ -106,14 +128,14 @@ class DataLoader(object):
     def load(self,save_path):
         with open(save_path,'rb') as f:
             self.dict_all=pickle.load(f)
-        self.ratings_matrix =self.dict_all['raw_ratings']
+        #self.ratings_matrix =self.dict_all['raw_ratings']
         self.ratings_predict=self.dict_all['new_ratings']
         self.price_dict=self.dict_all['prices']
         self.cate_dict=self.dict_all['cate']
-        self.user_mapper=self.dict_all['user_mapper']
-        self.item_mapper=self.dict_all['item_mapper']
-        self.user_inverse_mapper=self.dict_all['user_inverse_mapper']
-        self.item_inverse_mapper=self.dict_all['item_inverse_mapper']
+        #self.user_mapper=self.dict_all['user_mapper']
+        #self.item_mapper=self.dict_all['item_mapper']
+        #self.user_inverse_mapper=self.dict_all['user_inverse_mapper']
+        #self.item_inverse_mapper=self.dict_all['item_inverse_mapper']
         del self.dict_all
         print("Saved data loaded.")
 
@@ -123,8 +145,12 @@ class MBRecsys(object):
         self.top_value=top_value
         
     def predict(self):
+        self.train_R
         U, s, VT = svds(self.train_R, k = self.top_value)  #select top 15 sigular value
         S=np.diag(s)
-        self.out_R= np.dot(np.dot(U, S), VT)
+        self.out_R= np.dot(np.dot(U, S), VT)+3
+        self.out_R= self.out_R- np.minimum(self.out_R,0)
+        self.out_R= self.out_R+5- np.maximum(self.out_R,5)
+        
         return self.out_R
     
